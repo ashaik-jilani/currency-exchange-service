0089aebf2afe236c41a1af4d04a59b6d1280cbad
ashaik-jilani <45486687+ashaik-jilani@users.noreply.github.com>
2019-03-15 21:46:22 +0400

    Updated with Sleuth for easy tracing in Logging

--- src/main/java/com/supertech/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/supertech/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -3,8 +3,8 @@
  */
 package com.supertech.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private Environment environment;
@@ -32,6 +34,7 @@ public class CurrencyExchangeController {
 		
 //		ExchangeValue exchangeValue = new ExchangeValue("10001","USD", "INR", BigDecimal.valueOf(65));
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("Currency-Exchange ExchangeValue {} -> ", exchangeValue);
 		return exchangeValue;
 		
 	}
